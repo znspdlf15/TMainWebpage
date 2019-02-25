42a0fd19f1ec1811da4e51b52f653bc4eaf27d86
william.lee <lyt0915@naver.com>
2019-02-25 21:49:33 +0900

    push inner HTML

---
+++ js/common.js
@@ -0,0 +1,3 @@
+function getTagInnerHTML(address){
+  return '<object type="text/html" data="' + address +'" ></object>';
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-function test_func(self){
-  alert(self.tag + " 실행됨.");
+function click_search_button(self){
+  var search_text = document.getElementById('search_text');
+  var main_area = document.getElementById('content');
+
+  main_area.innerHTML = "안녕하세요";
+}
+
+function click_self_introduction(){
+  var data = JSON.parse(directory);
+  var address = data[0]["ProfileFile"];
+
+  var main_area = document.getElementById('content');
+  main_area.innerHTML = getTagInnerHTML(address);
 }
